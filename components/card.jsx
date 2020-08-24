cf9931b4cb45c6e130469ff666dc29fedcbefb37
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-08-24 11:54:51 -0500

    Remove 'truncate' from cards title and description

--- components/card.jsx
+++ components/card.jsx
@@ -1,6 +1,5 @@
 import LaunchIcon from "../components/icons/launch";
 import IconButton from "../components/iconButton";
-import { truncate } from "lodash";
 
 const Card = ({ url, imgSrc, title, description, tags }) => (
   <div
@@ -9,14 +8,10 @@ const Card = ({ url, imgSrc, title, description, tags }) => (
   >
     {imgSrc ? <img className="w-full" src={imgSrc} alt={title}></img> : ""}
     <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">
-        {truncate(title, { length: 30, omission: "..." })}
-      </div>
-      <p className="text-gray-600 text-base">
-        {truncate(description, { length: 100, omission: "..." })}
-      </p>
+      <div className="font-bold text-xl mb-2">{title}</div>
+      <p className="text-gray-600 text-base">{description}</p>
     </div>
-    <div className="h-32"></div>
+    <div className="h-24"></div>
     <div
       className="text-sm flex flex-row items-center w-9/12 mr-4 flex-wrap absolute"
       style={{ left: "1.5rem", bottom: "1.5rem" }}
