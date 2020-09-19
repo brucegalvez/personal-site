086a7d72561f6ec397a7ed7633c91413a2b51801
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-09-18 22:37:48 -0500

    Change LangButtons component name

--- components/LangButtons.jsx
+++ components/LangButtons.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 
-const Language = ({ setLanguage }) => (
+const LangButtons = ({ setLanguage }) => (
   <div
     className="flex flex-col fixed z-20"
     style={{ right: "1rem", bottom: 0 }}
   >
     {["es", "en"].map((option) => (
       <button
-        className="rounded-full text-xs uppercase text-pink-500 border-pink-500 border-2
+        className="rounded-full text-xs uppercase 
+        text-pink-500 border-pink-500 border-2
         hover:bg-pink-500 hover:text-gray-800 transform duration-200
         h-8 w-8 mb-4 flex items-center justify-center"
         key={option}
@@ -19,8 +20,8 @@ const Language = ({ setLanguage }) => (
   </div>
 );
 
-Language.propTypes = {
+LangButtons.propTypes = {
   setLanguage: PropTypes.func.isRequired,
 };
 
-export default Language;
+export default LangButtons;
