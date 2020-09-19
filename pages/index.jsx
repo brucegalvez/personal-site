f57af7392e6430854cae6aea9a6e284910d6d2c0
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-09-18 22:40:48 -0500

    Pass ScrollToSection to AppBar and Drawer and add debounce func to HomePage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react";
-import Header from "../components/header";
-import AppBar from "../components/appBar";
-import Drawer from "../components/drawer";
-import Language from "../components/language";
+import Header from "../components/Header";
+import AppBar from "../components/AppBar";
+import Drawer from "../components/Drawer";
+import LangButtons from "../components/LangButtons";
 import HamburguerIcon from "../components/icons/hamburguer";
-import MastHead from "../components/sections/masthead";
-import AboutMe from "../components/sections/aboutMe";
-import SoftwareProjects from "../components/sections/softwareProjects";
-import CreativeProjects from "../components/sections/creativeProjects";
-import Contact from "../components/sections/contact";
-import Footer from "../components/footer";
+import MastHead from "../components/sections/Masthead";
+import AboutMe from "../components/sections/AboutMe";
+import SoftwareProjects from "../components/sections/SoftwareProjects";
+import CreativeProjects from "../components/sections/CreativeProjects";
+import Contact from "../components/sections/Contact";
+import Footer from "../components/Footer";
 import contents from "../contents";
-
-function debounce(fn, ms) {
-  let timer;
-  return (_) => {
-    clearTimeout(timer);
-    timer = setTimeout((_) => {
-      timer = null;
-      fn.apply(this, arguments);
-    }, ms);
-  };
-}
 
 const HomePage = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [language, setLanguage] = useState("en");
   const [dimensions, setDimensions] = useState({ height: 0, width: 0 });
 
-  const scrollToSection = (id) => {
-    document.querySelector("." + id).scrollIntoView({
+  const debounce = (fn, ms) => {
+    let timer;
+    return (_) => {
+      clearTimeout(timer);
+      timer = setTimeout((_) => {
+        timer = null;
+        fn.apply(this, arguments);
+      }, ms);
+    };
+  };
+
+  const scrollToSection = (selector) => {
+    document.querySelector(selector).scrollIntoView({
       behavior: "smooth",
     });
   };
@@ -53,14 +53,14 @@ const HomePage = () => {
     <div className="bg-gray-800 font-mono text-gray-200">
       <Header siteTitle={contents.mainTexts.siteTitle[language]} />
       <AppBar
-        isDrawerOpen={isDrawerOpen}
-        setIsDrawerOpen={setIsDrawerOpen}
         sections={contents.mainTexts.sectionTitles}
         language={language}
+        scrollToSection={scrollToSection}
       />
       <Drawer
         isDrawerOpen={isDrawerOpen}
         sections={contents.mainTexts.sectionTitles}
+        scrollToSection={scrollToSection}
         language={language}
       />
       <HamburguerIcon
@@ -69,7 +69,7 @@ const HomePage = () => {
         className="mr-8 md:hidden fixed right-0 z-20"
         style={{ top: "1rem" }}
       />
-      <Language setLanguage={setLanguage} />
+      <LangButtons setLanguage={setLanguage} />
       <MastHead language={language} contents={contents} />
       <AboutMe language={language} contents={contents} />
       <SoftwareProjects language={language} contents={contents} />
